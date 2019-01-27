4c34653940af2c97cd0ee9f40f84dacca3582851
cookavsky <cookavsky@gmail.com>
2019-01-28 00:54:25 +0100

    -Create component platform, league, table, result - create style for button in platform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlatformComponent } from './platform/platform.component';
+import { LeagueComponent } from './league/league.component';
+import { TableComponent } from './table/table.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlatformComponent,
+    LeagueComponent,
+    TableComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
