d62eea4242fcae6600d6033136e8a3143e1cd7bb
cookavsky <cookavsky@gmail.com>
2019-01-31 00:04:15 +0100

    -hidden table and result

--- src/app/league/league.component.ts
+++ src/app/league/league.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ToggleService } from '../toggle.service';
+import { containerRefreshStart } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-league',
@@ -8,8 +9,18 @@ import { ToggleService } from '../toggle.service';
   styleUrls: ['./league.component.css']
 })
 export class LeagueComponent implements OnInit {
+  id = 1;
+  containers = [];
 
   constructor(public toggleService: ToggleService) { }
+
+  add() {
+    this.containers.push(this.containers.length);
+  }
+
+  delete() {
+    this.containers.pop();
+  }
 
   ngOnInit() {
   }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -7,8 +7,27 @@ import { ToggleService } from '../toggle.service';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  public hiddenName: boolean = false;
+  name: string;
+  Nr = 1;
+  containers = [];
 
   constructor(public toggleService: ToggleService) { }
+
+  add(): void {
+    this.containers.push(this.containers);
+  }
+
+  delete(): void {
+    this.containers.pop();
+  }
+
+  accept(): void {
+    if (this.hiddenName === false && this.name !== undefined)
+    {
+      this.hiddenName = !this.hiddenName;
+    }
+  }
 
   ngOnInit() {
   }
